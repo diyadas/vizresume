ebcac283998cbe1d8589a232adb642e642d2f3a5
Diya Das <diyadas@users.noreply.github.com>
2018-11-22 00:02:07 -0800

    load from Google Sheets instead

--- vizresume.R
+++ vizresume.R
@@ -7,19 +7,21 @@ library(ggplot2)
 library(scales)
 library(grid)
 
-df <- data.frame(
-  x1 = c("Sep 2008", "Aug 2012", "May 2018", "Sep 2012", "Mar 2013", "Jan 2014"),
-  x2 = c("Jun 2012", "May 2018", "Nov 2018", "May 2015", "May 2015", "May 2015"),
-  y1 = c(0, 0, 0, 0, 1, 2),
-  y2 = c(3, 3, 3, 1, 2, 3),
-  category = c(rep("Academic Position", 3), rep("Community Involvement", 3)),
-  rect_labels = c("AB\nPrinceton University", "PhD\nUC Berkeley", 
-                  "Postdoc\nUC Berkeley", "Finance Committee, EYH", 
-                  "Finance Agent, EYH", "Signatory, EYH"),
-  org = c("Princeton University", "UC Berkeley", "UC Berkeley", "EYH", "EYH", "EYH"))
+# df <- data.frame(
+#   start = c("Sep 2008", "Aug 2012", "May 2018", "Sep 2012", "Mar 2013", "Jan 2014"),
+#   end = c("Jun 2012", "May 2018", "Nov 2018", "May 2015", "May 2015", "May 2015"),
+#   y1 = c(0, 0, 0, 0, 1, 2),
+#   y2 = c(3, 3, 3, 1, 2, 3),
+#   category = c(rep("Academic Position", 3), rep("Community Involvement", 3)),
+#   name = c("AB\nPrinceton University", "PhD\nUC Berkeley", 
+#                   "Postdoc\nUC Berkeley", "Finance Committee, EYH", 
+#                   "Finance Agent, EYH", "Signatory, EYH"),
+#   org = c("Princeton University", "UC Berkeley", "UC Berkeley", "EYH", "EYH", "EYH"))
 
-df_rect <- df %>% filter(as.character(x1) != as.character(x2))
-df_point <- df %>% filter(as.character(x1) == as.character(x2))
+df <- gs_read(ss = gs_title("Resume - projects and achievements"), ws = "Sheet1")
+
+df_rect <- df %>% filter(as.character(start) != as.character(end))
+df_point <- df %>% filter(as.character(start) == as.character(end))
 
 no_style <- theme(axis.ticks.y = element_blank(),
         axis.text.y = element_blank(),
@@ -29,14 +31,14 @@ no_style <- theme(axis.ticks.y = element_blank(),
 p <- ggplot() +
   scale_x_date(labels = date_format("%m-%Y")) +
   geom_rect(data = df_rect, 
-            mapping = aes(xmin = as.Date(paste(1, x1), "%d %b %Y"),
-                          xmax = as.Date(paste(1, x2), "%d %b %Y"),
+            mapping = aes(xmin = as.Date(paste(1, start), "%d %b %Y"),
+                          xmax = as.Date(paste(1, end), "%d %b %Y"),
                           ymin = y1, ymax = y2, fill = org),
             color = "black", alpha = 0.5) +
-  geom_text(data = df_rect, aes(x = as.Date(paste(1, x1), "%d %b %Y") +
-                             (as.Date(paste(1, x2), "%d %b %Y") -
-                                as.Date(paste(1, x1), "%d %b %Y")) / 2,
-                           y = y1 + (y2 - y1) / 2, label = rect_labels),
+  geom_text(data = df_rect, aes(x = as.Date(paste(1, start), "%d %b %Y") +
+                             (as.Date(paste(1, end), "%d %b %Y") -
+                                as.Date(paste(1, start), "%d %b %Y")) / 2,
+                           y = y1 + (y2 - y1) / 2, label = name),
             size = 4) + facet_grid(category ~ ., switch = "y") +
   labs(x = "Year", y = "")  + no_style
 p
