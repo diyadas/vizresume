5077aac78fd96f5ec62ee7c38094b770e2d4013d
Diya Das <diyadas@users.noreply.github.com>
2018-12-05 00:31:46 -0800

    point-events represented with font awesome

--- vizresume.R
+++ vizresume.R
@@ -8,6 +8,15 @@ library(scales)
 library(grid)
 library(googlesheets)
 library(stringr)
+library(emojifont)
+
+load.fontawesome(font = "Font Awesome 5 Free-Solid-900.otf") #icons 
+fa <- data.frame(type = c("Award", "Poster", "Publication", 
+                           "Software", "Talk", "Workshop"),
+                 fnt = fontawesome(c('fa-trophy', 'fa-pie-chart', 
+                                     'fa-pencil', 
+                                   'fa-code', 'fa-comment', 
+                                   'fa-wrench')))
 
 df <- gs_read(ss = gs_title("Resume - projects and achievements"), 
               ws = "Sheet1")
@@ -27,6 +36,8 @@ for (x in unique(df$org)) df <- add_row(df, category = df$category[1],
 
 df_rect <- df %>% filter(as.character(start) != as.character(end) | is.na(start))
 df_point <- df %>% filter(as.character(start) == as.character(end) | is.na(start))
+df_point <- left_join(df_point, fa)
+
 
 no_style <- theme(axis.ticks.y = element_blank(),
         axis.text.y = element_blank(),
@@ -45,11 +56,12 @@ p <- ggplot() +
                                 as.Date(paste(1, start), "%d %b %Y")) / 2,
                            y = y1n + (y2n - y1n) / 2, 
                            label = name_wrap), size = 2.8, lineheight = 0.7) + 
-  geom_point(data = df_point,
-             mapping = aes(x = as.Date(paste(1, start), "%d %b %Y"),
-                           y = (y1n + y2n)/2, 
-                           colour = wrap_format(28)(org),
-                           shape = type)) + 
+  geom_text(data = df_point,  aes(x = as.Date(paste(1, start), "%d %b %Y"),
+                                  y = (y1n + y2n)/2, 
+                                  colour = wrap_format(28)(org),
+                                  label = fnt), 
+            alpha = 0.5,
+            family='fontawesome-webfont') +
   scale_colour_discrete(guide = "none") + 
   facet_grid(category ~ ., switch = "y",
              labeller = label_wrap_gen(width = 20)) +
