3da6daecffaf2c86025d818388df944aef213fb0
Diya Das <diyadas@users.noreply.github.com>
2018-12-08 23:20:26 -0800

    rewrite this to plot organization legend and a second legend for fa icons below the main plot, using grid.arrange and grobs

--- vizresume.R
+++ vizresume.R
@@ -1,6 +1,6 @@
 # Title: Visual Resume (Timelines)
 # Author: Diya Das
-# Last revised: Tue Dec  4 18:06:34 2018
+# Last revised: Sat Dec  8 23:16:53 2018
 
 library(dplyr)
 library(ggplot2)
@@ -9,6 +9,8 @@ library(grid)
 library(googlesheets)
 library(stringr)
 library(emojifont)
+library(cowplot)
+library(gridExtra)
 
 load.fontawesome(font = "Font Awesome 5 Free-Solid-900.otf") #icons 
 fa <- data.frame(type = c("Award", "Poster", "Publication", 
@@ -38,19 +40,26 @@ df_rect <- df %>% filter(as.character(start) != as.character(end) | is.na(start)
 df_point <- df %>% filter(as.character(start) == as.character(end) | is.na(start))
 df_point <- left_join(df_point, fa)
 
-
 no_style <- theme(axis.ticks.y = element_blank(),
         axis.text.y = element_blank(),
-        panel.background = element_blank())
+        axis.ticks.y.left = element_blank(),
+        axis.title.y = element_blank(),
+        strip.text = element_text(face="bold"),
+        text = element_text(size=8),
+        axis.text.x=element_text(size=8),
+        plot.margin=unit(c(0,0,5.5,0),"pt"),
+        plot.title = element_blank()
+        ) 
 
 p <- ggplot() +
-  scale_x_date(labels = date_format("%m-%Y")) +
+  scale_x_date(labels = date_format("%Y"), date_breaks = "1 year") +
   geom_rect(data = df_rect, 
             mapping = aes(xmin = as.Date(paste(1, start), "%d %b %Y"),
                           xmax = as.Date(paste(1, end), "%d %b %Y"),
                           ymin = y1n, ymax = y2n, 
-                          fill = wrap_format(28)(org)),
-            color = "grey", alpha = 0.5) + labs(fill='Organization') +
+                          fill = wrap_format(40)(org)),
+            color = "grey", alpha = 0.5) + labs(fill='Organization') + 
+  guides(fill = guide_legend(ncol=2)) +
   geom_text(data = df_rect, aes(x = as.Date(paste(1, start), "%d %b %Y") +
                              (as.Date(paste(1, end), "%d %b %Y") -
                                 as.Date(paste(1, start), "%d %b %Y")) / 2,
@@ -58,13 +67,40 @@ p <- ggplot() +
                            label = name_wrap), size = 2.8, lineheight = 0.7) + 
   geom_text(data = df_point,  aes(x = as.Date(paste(1, start), "%d %b %Y"),
                                   y = (y1n + y2n)/2, 
-                                  colour = wrap_format(28)(org),
+                                  colour = wrap_format(40)(org),
                                   label = fnt), 
-            alpha = 0.5,
+            alpha = 0.5, family='fontawesome-webfont') +
+  scale_colour_discrete(guide = 'none') + 
+  facet_grid(category ~ ., switch = "y", labeller = label_wrap_gen(width = 20)) +
+  labs(x = "Year", y = "")  + no_style 
+
+# Legend Extraction Code from Luciano Selzer
+## https://stackoverflow.com/a/11886071/743568
+g_legend <- function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)}
+legend <- g_legend(p)
+
+annot_point <- data.frame(fa, x = rep(0, nrow(fa)), y = nrow(fa):1)
+
+icon_plot <- ggplot(data.frame()) + geom_point() + xlim(0, 1) + ylim(0.5, 6.5)+
+  geom_text(data = annot_point,  aes(x = x,
+                                     y = y,
+                                     label = fnt), alpha = 0.5,
             family='fontawesome-webfont') +
-  scale_colour_discrete(guide = "none") + 
-  facet_grid(category ~ ., switch = "y",
-             labeller = label_wrap_gen(width = 20)) +
-  labs(x = "Year", y = "")  + no_style
-p
+  geom_text(data = annot_point,  aes(x = x + 0.1,
+                                     y = y,
+                                     label = type),
+            hjust = 0, size = 2.8) + theme_nothing() 
+
+quartz(width = 9.5 , height = 6.6)
+lay <- rbind(c(1,1,1),
+             c(2,2,3))
+grid.arrange(grobs = list(ggplotGrob(p + theme(legend.position = "none")),
+                          legend,
+                          ggplotGrob(icon_plot)), 
+             layout_matrix = lay,
+             heights = unit(c(5.2, 1), c("in", "in")))
 
